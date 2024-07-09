b215f35dc4d9d50316546b7a3427289b1bd57162
Juan Marenco <marencojuan178@gmail.com>
2024-07-09 13:16:23 -0500

    Ajustes en filtros y colores

--- update.sh
+++ update.sh
@@ -6,9 +6,11 @@ RESET="\e[0m"
 LOG_DIR="/var/log/apt/info_actualizacion"
 LOG_FILE="$LOG_DIR/informe.txt"
 
-echo -e "${GREEN}        -----------------LINUX UPGRADE -----------------------${GREEN}"
+echo -e "${GREEN}        -----------------LINUX UPGRADE -----------------------${RESET}"
 echo "   DEBES EJECUTAR ESTE SCRIPT COMO SUPER-USUARIO, MAS ADELANTE TE QUITARÉ ESE PASO :3, SI NO ES ASI, VUELVE A EJECUTAR EL SCRIPT COMO 
-   CON SUPER-USUARIO   " 
+   CON SUPER-USUARIO"
+echo "Ejecuta el comando de la siguiente forma: sudo ./update.sh" 
+
 echo ""
 echo -e "${GREEN}     ¿DESEA ACTUALIZAR TODO LOS PAQUETES Y REPOSITORIOS?     ${RESET}"
 
@@ -30,7 +32,7 @@ then
             clear
             echo "                  Ok! INICIADO ACTUALIZACIÓN                "
             sudo apt update -y
-            mkdir /var/log/apt/info_actualizacion
+            mkdir $LOG_DIR
             sudo apt full-upgrade -y &> $LOG_FILE
             if sudo apt full-upgrade -y | grep -q "0 actualizados, 0 nuevos se instalarán, 0 para eliminar y 0 no actualizados."
             then
@@ -38,7 +40,7 @@ then
                 echo -e "${RED}     ---- SIN ACTUALIZACIONES DISPONIBLES --- ${RESET}"
                 echo "       INFORME GUARDADO EN LA CARPETA /var/log/apt                   "
             else
-                echo "${GREEN}      --- SISTEMA ACTUALIZADO CON EXITO ---    ${RESET}"
+                echo "--- SISTEMA ACTUALIZADO CON EXITO --- "
                 echo "       INFORME GUARDADO EN LA CARPETA /var/log/apt                   "
             fi
             ;;
@@ -47,17 +49,17 @@ then
             echo "Ok!, Solo se hará las actualizaciónes"
             sudo apt update -y
             sudo apt full-upgrade -y 
-            if sudo apt full-upgrade -y | grep -q "0 actualizados, 0 nuevos se instalarán, 0 para eliminar y 0 no actualizados." | grep -q "Todos los paquetes están actualizados."
+            if sudo apt update -y | grep -q "All packages are up to date." 
             then
-                echo "${RED}      ----- SIN ACTUALIZACIONES DISPONIBLE -----     ${RESET}"
+                echo -e"${RED} ------- SIN ACTUALIZACIONES DISPONIBLE --------- ${RED}"
             else
-                echo -e "${GREEN}  ----- SISTEMA ACTUALIZADO CON EXITO ----- ${RESET}"
+                echo -e "${GREEN} ------- SISTEMA ACTUALIZADO CON EXITO ------- ${RESET}"
             fi
             ;;
         *) 
-            echo -e "${RED} ----- OPCIÓN INVÁLIDA -----  ${RESET}"
+            echo -e "${RED} ----- OPCIÓN INVÁLIDA ----- ${RED}"
     esac
 elif [[ "$input" = "n" ]]
 then
-    echo -e "${RED} ------------ NO SE ACTUALIZARÁ EL SISTEMA ----------------- ${RESET}"
+    echo -e "${RED} ------------ NO SE ACTUALIZARÁ EL SISTEMA ----------------- ${RED}"
 fi
